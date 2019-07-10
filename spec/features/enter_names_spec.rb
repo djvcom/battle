b1b21147715ad192d1895e7949792a6fd5c623a5
djvcom <djverrall@gmail.com>
2019-07-10 15:08:32 +0100

    Extracted a method from the tests

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,10 +1,7 @@
 
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_one_name, with: 'Dave'
-    fill_in :player_two_name, with: 'Mittens'
-    click_button 'Submit'
-    expect(page).to have_content 'Dave vs. Mittens'
+    sign_in_and_play
+    expect(page).to have_content 'Dave :60 HP vs. Mittens :60 HP'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,10 +1,7 @@
 
 feature 'Display Hit Points' do
   scenario 'displaying player 2s hit points' do
-    visit('/')
-    fill_in :player_one_name, with: 'Dave'
-    fill_in :player_two_name, with: 'Mittens'
-    click_button 'Submit'
-    expect(page).to have_content 'Mittens - 30'
+    sign_in_and_play
+    expect(page).to have_content 'Mittens :60 HP'
   end
 end
